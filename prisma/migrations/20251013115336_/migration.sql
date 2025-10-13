18ebde9a99db73b2ef1582bf4b064357e2e5e9fa
Данила Квашнёв <kvasnevdanila@MacBook-Air-Danila.local>
2025-10-13 14:57:14 +0300

    prisma postgreSQL

---
+++ prisma/migrations/20251013115336_/migration.sql
@@ -0,0 +1,48 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "telegramId" INTEGER NOT NULL,
+    "username" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Homework" (
+    "id" SERIAL NOT NULL,
+    "subject" TEXT NOT NULL,
+    "text" TEXT NOT NULL,
+    "deadline" TIMESTAMP(3) NOT NULL,
+    "done" BOOLEAN NOT NULL DEFAULT false,
+    "userId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Homework_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Schedule" (
+    "id" SERIAL NOT NULL,
+    "dayOfWeek" INTEGER NOT NULL,
+    "subject" TEXT NOT NULL,
+    "time" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Schedule_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_telegramId_key" ON "User"("telegramId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Homework_userId_key" ON "Homework"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Schedule_userId_key" ON "Schedule"("userId");
+
+-- AddForeignKey
+ALTER TABLE "Homework" ADD CONSTRAINT "Homework_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Schedule" ADD CONSTRAINT "Schedule_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
